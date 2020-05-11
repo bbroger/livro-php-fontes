eec1279f5a11e8117c1fc868103306b38f7b6125
Ribamar FS <ribafs@gmail.com>
2020-05-11 19:42:18 -0300

    Update index.php

--- Paginacao/paginacao_simples/index.php
+++ Paginacao/paginacao_simples/index.php
@@ -47,7 +47,7 @@ $total_pages = ceil($rows[0]/$row_limit);
     </div>
 </div>
     
-<script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
+<script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
 <script src="js/jquery.bootpag.min.js" type="text/javascript"></script>
 
